8ae8b43ce0432988bd5bf717dd7bf62e058c5888
Blade Boles <bladerboles@gmail.com>
2020-03-02 10:30:56 -0700

    Finished 2 and 3

--- recursive.js
+++ recursive.js
@@ -1,3 +1,4 @@
+//Counting Sheep
 const countingSheep = function(flock) {
     if(flock === 0) {
         return console.log('All sheep jumped over the fence!')
@@ -8,6 +9,8 @@ const countingSheep = function(flock) {
 
 countingSheep(3)
 
+
+// Power Calculator
 const powerCalculator = function (base, exponent) {
     if (exponent < 0) {
         return console.log('exponent should be >= 0')
@@ -18,3 +21,15 @@ const powerCalculator = function (base, exponent) {
     }
 }
 console.log(powerCalculator(10, 3))
+
+// Reverse String
+
+const reverseString = function (str) {
+    if (str.length === 0) {
+        return '';
+    } else {
+        return str[str.length - 1] + reverseString(str.slice(0, (str.length - 1) ))
+    }
+}
+
+console.log(reverseString('Billy'))
