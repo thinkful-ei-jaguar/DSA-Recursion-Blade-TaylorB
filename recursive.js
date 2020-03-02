7a83d31f20b603528bbee465016c0f14a256a701
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-03-02 15:11:10 -0500

    tried string splitter

--- recursive.js
+++ recursive.js
@@ -9,7 +9,6 @@ const countingSheep = function(flock) {
 
 countingSheep(3)
 
-
 // Power Calculator
 const powerCalculator = function (base, exponent) {
     if (exponent < 0) {
@@ -23,7 +22,6 @@ const powerCalculator = function (base, exponent) {
 console.log(powerCalculator(10, 3))
 
 // Reverse String
-
 const reverseString = function (str) {
     if (str.length === 0) {
         return '';
@@ -33,3 +31,42 @@ const reverseString = function (str) {
 }
 
 console.log(reverseString('Billy'))
+
+const nthTriangular = function(num) {
+    if (num === 1) {
+        return 1
+    }
+    return num + nthTriangular(num -1)
+}
+
+console.log(nthTriangular(6))
+
+//Split without Array.split()
+const stringSplitter = function(string, separator) {
+    if (string.length === 0) {
+        return []
+    }
+    let newString = ''
+    let reducedString = ''
+    let newArray = []
+    for (i = 0; i < string.length; i++) {
+        if (string[i] !== separator) {
+             newString = newString + string[i]
+            console.log('newString', newString)
+        }
+        else if (string[i] === string[string.length - 1]) {
+            newArray = [newString, ...newArray]
+        }
+        else {
+            reducedString = string.slice(i + 1)
+            console.log('reducedString', reducedString)
+            newArray  = [newString, ...stringSplitter( reducedString, separator)]
+        }
+    }
+    console.log('newArray console =>', newArray)    
+    return newArray
+} 
+
+console.log(stringSplitter('16/45/2378', '/'))
+
+// return [ newString, ...stringSplitter(reducedString, sameSeparator)]
