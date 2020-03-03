a50d9ede4d0fd57f14345783936fe0995d8ceea8
Blade Boles <bladerboles@gmail.com>
2020-03-02 17:58:59 -0700

    Solved first maze

--- recursive.js
+++ recursive.js
@@ -43,16 +43,16 @@ console.log(nthTriangular(6));
 
 //Split without Array.split()
 const stringSplitter = function(string, separator) {
-    let sepIndex = string.indexOf(separator);
+  let sepIndex = string.indexOf(separator);
     
-    if (sepIndex === -1) {
-        return [string];
-    } else {
-        return [string.slice(0, sepIndex)].concat(stringSplitter(string.slice(sepIndex + separator.length), separator))
-    }
+  if (sepIndex === -1) {
+    return [string];
+  } else {
+    return [string.slice(0, sepIndex)].concat(stringSplitter(string.slice(sepIndex + separator.length), separator));
+  }
 
-} 
-console.log(stringSplitter('16/45/2378', '/'))
+}; 
+console.log(stringSplitter('16/45/2378', '/'));
 
 //Fibonacci 
 
@@ -63,57 +63,113 @@ const fibo = function (number) {
   if (number <= 2) {
     return 1;
   } else {
-    return fibo(number - 1) + fibo(number - 2)};
+    return fibo(number - 1) + fibo(number - 2);}
 };
 
-const arr = []
-let fibNum = 5
+const arr = [];
+let fibNum = 5;
 
 for (let i = 1; i <= fibNum; i++) { 
-        arr.push(fibo(i)) 
-    } 
+  arr.push(fibo(i)); 
+} 
 
 console.log(arr);
 
 //Factorial
 const factorialFinder = function(num) {
-    if (num <= 1) {
-        return 1
-    }
+  if (num <= 1) {
+    return 1;
+  }
 
-    return num * factorialFinder(num - 1)
-}
+  return num * factorialFinder(num - 1);
+};
 
-console.log(factorialFinder(5))
+console.log(factorialFinder(5));
 
 //Maze
+
+
+
 let mySmallMaze = [
-    [' ', ' ', ' '],
-    [' ', '*', ' '],
-    [' ', ' ', 'e']
+  [' ', ' ', ' '],
+  [' ', '*', ' '],
+  [' ', ' ', 'e']
 ];
 
-let maze = [
-    [' ', ' ', ' ', '*', ' ', ' ', ' '],
-    ['*', '*', ' ', '*', ' ', '*', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', '*', '*', '*', '*', '*', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', 'e']
+let bigMaze = [
+  [' ', ' ', ' ', '*', ' ', ' ', ' '],
+  ['*', '*', ' ', '*', ' ', '*', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', '*', '*', '*', '*', '*', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', 'e']
 ];
 
-const aMazeingTime = function (maze, indexRow, indexColumn, route) {
-   const route = ''
-   if(maze[indexRow][indexColumn] === 'e'){
-       return 'Yay we done!'//go back to this later
-   }
-   if(maze[indexRow][indexColumn] === '*'){
-        if(route[route.length -1] === 'D') {
-            route[route.length] = 'U'
-        }
-   }
-}
+let trickyMaze = [
+  [' ', ' ', ' ', '*', ' ', ' ', ' '],
+  ['*', '*', ' ', '*', ' ', '*', ' '],
+  [' ', ' ', ' ', ' ', '*', ' ', ' '],
+  [' ', '*', '*', '*', '*', '*', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', 'e']
+];
+
+const aMaze = function (maze, Row, Column) {
+   
+  if(maze[Row][Column] === 'e'){
+    console.log(maze)
+
+  } else {
+    maze[Row][Column] = '+'
+
+    if (Row < maze.length - 1) {
+      if(maze[Row + 1][Column] === ' ' || maze[Row + 1][Column] === 'e') {
+        return aMaze(maze, (Row + 1), Column)
+      };
+    }
+    if(Column < maze[Row].length - 1) {
+      if (maze[Row][Column + 1] === ' ' || maze[Row][Column + 1] === 'e') {
+      return aMaze(maze, Row, (Column + 1))
+    };
+    }
+    if(Row > 0) {
+      if (maze[Row - 1][Column] === ' ' || maze[Row - 1][Column] === 'e') {
+      return aMaze(maze, (Row - 1), Column)
+    };
+    }
+    if(Column > 0) {
+      if (maze[Row][Column - 1] === ' ' || maze[Row][Column - 1] === 'e') {
+      return aMaze(maze, Row, (Column - 1))
+    };
+    }
+
+    maze[Row][Column] = 'X'
+    
+    if (Row < maze.length - 1) {
+      if(maze[Row + 1][Column] === '+') {
+        return aMaze(maze, (Row + 1), Column)
+      };
+    }
+    if(Column < maze[Row].length - 1) {
+      if (maze[Row][Column + 1] === '+') {
+      return aMaze(maze, Row, (Column + 1))
+    };
+    }
+    if(Row > 0) {
+      if (maze[Row - 1][Column] === '+') {
+      return aMaze(maze, (Row - 1), Column)
+    };
+    }
+    if(Column > 0) {
+      if (maze[Row][Column - 1] === '+') {
+      return aMaze(maze, Row, (Column - 1))
+    };
+    }
+  
+  }
+   
+};
+
+aMaze(trickyMaze, 0, 0)
 
 //if maze[currentIndex + 1][currentIndex + 1] === ' '
 //currentIndex = currentIndex + 1 
 
-const obj = {Zuckerberg: {Schroepfer: }}
